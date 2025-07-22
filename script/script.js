2e958ef64b5466440c399be1f5c8f22f12dcc555
AnaCeciliaBane <anacegsbane@gmail.com>
2025-07-22 15:42:17 -0300

    mensagem da atualização

--- script/script.js
+++ script/script.js
@@ -3,24 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (contactForm) {
         contactForm.addEventListener('submit', function (event) {
-            event.preventDefault(); // Impede o envio real do formulário
+            event.preventDefault(); 
 
-            // Obter os valores dos campos
             const name = document.getElementById('name').value.trim();
             const email = document.getElementById('email').value.trim();
             const subject = document.getElementById('subject').value.trim();
             const message = document.getElementById('message').value.trim();
 
-            // Validação básica
+           
             if (!name || !email || !subject || !message) {
                 alert('Por favor, preencha todos os campos do formulário.');
                 return;
             }
 
-            // Exibe o alerta de sucesso
+            
             alert('Mensagem enviada com sucesso!');
 
-            // Limpa o formulário
+        
             contactForm.reset();
         });
     }
